5ed6cd3bea9ce3d844ed634e1d97737afca89b78
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-17 17:55:06 -0300

    Atividade finalizada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,18 +10,23 @@ public class Main {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
     Executor executor = new Executor(entrada);
-    List<Map<String, Object>> listaFuncionarios = new ArrayList<>();
+    List<FuncionarioPadrao> listaFuncionarios = new ArrayList<>();
 
     int opcaoMenu;
     do {
       opcaoMenu = executor.menuPrincipal();
-      Map<String, Object> dadosFuncionario;
+      FuncionarioPadrao funcionario;
 
       switch (opcaoMenu) {
         case 1:
-          dadosFuncionario = executor.cadastroPadrao();
-          listaFuncionarios.add(dadosFuncionario);
-          break;
+          funcionario = executor.cadastroPadrao();
+          if (!executor.VerificarMatriculaExistente(listaFuncionarios, funcionario.matricula)) {
+            listaFuncionarios.add(funcionario);
+            break;
+          } else {
+            System.out.println("Matrícula já cadastrada");
+            break;
+          }
 
         case 2:
           executor.listarFuncionarios(listaFuncionarios);
@@ -30,7 +36,15 @@ public class Main {
           break;
 
         default:
-          break;
+          System.out.println("Opção inváida, por favor escolha uma das opções acima!");
+      }
+
+      boolean continuar = executor.continuarOperacao();
+      if (continuar) {
+        continue;
+      } else {
+        System.out.println("Até a próxima!");
+        return;
       }
     } while (opcaoMenu != 5);
 
@@ -53,10 +67,23 @@ public class Executor {
     return opcaoMenu;
   }
 
-  public void listarFuncionarios(List<Map<String, Object>> funcionarios) {
+  public boolean VerificarMatriculaExistente(List<FuncionarioPadrao> funcionarios, int matricula) {
+    for (FuncionarioPadrao f : funcionarios) {
+      if (f.matricula == matricula) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void listarFuncionarios(List<FuncionarioPadrao> funcionarios) {
+    if (funcionarios.isEmpty()) {
+      System.out.println("Lista de funcionarios vazia");
+      return;
+    }
     System.out.println("======= LISTA DE FUNCIONÁRIOS =======");
 
-    for (Map<String, Object> f : funcionarios) {
+    for (FuncionarioPadrao f : funcionarios) {
       String mensagem = """
           ----------------------------------
           Nome: %s
@@ -65,59 +92,119 @@ public class Executor {
           Salário: R$ %.2f
           ----------------------------------
           """.formatted(
-          f.get("nome"),
-          f.get("matricula"),
-          f.get("tipo"),
-          f.get("salario"));
+          f.nome,
+          f.matricula,
+          f.tipo,
+          f.salario);
 
       System.out.println(mensagem);
     }
-
   }
 
-  public Map<String, Object> cadastroPadrao() {
+  public FuncionarioPadrao cadastroPadrao() {
     PrintElements.inputNome();
     String nome = scanner.nextLine();
 
     PrintElements.inputMatricula();
-    String matricula = scanner.nextLine();
+    int matricula = scanner.nextInt();
+    scanner.nextLine();
 
-    PrintElements.funcionarioInfo("padrão", nome, matricula, salario);
+    System.out.println("Funcionário CLT criado com sucesso!");
+    PrintElements.funcionarioInfo("CLT", nome, matricula, salario);
 
-    Map<String, Object> response = new HashMap<>();
-    response.put("tipo", "padrão");
-    response.put("nome", nome);
-    response.put("matricula", matricula);
-    response.put("salario", salario);
+    FuncionarioPadrao user = new FuncionarioPadrao("CLT", nome, matricula, salario);
+    return user;
+  }
 
-    return response;
+  public boolean continuarOperacao() {
+    while (true) {
+      PrintElements.cuntinuarOperacao();
+      String response = scanner.nextLine().trim().toLowerCase();
+
+      if (response.isEmpty()) {
+        System.out.println("Digite 's' para sim ou 'n' para não.");
+        continue;
+      }
+
+      char primeiraLetra = response.charAt(0);
+
+      if (primeiraLetra == 's') {
+        return true;
+      } else if (primeiraLetra == 'n') {
+        return false;
+      }
+
+      System.out.println("Digite 's' para sim ou 'n' para não.");
+    }
+  }
+}
+
+public class FuncionarioPadrao {
+  String tipo;
+  String nome;
+  int matricula;
+  double salario;
+
+  public FuncionarioPadrao(String tipo, String nome, int matricula, double salario) {
+    new Validator(tipo, matricula);
+
+    this.tipo = tipo;
+    this.nome = nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase();
+    this.matricula = matricula;
+    this.salario = salario;
+  }
+
+  class Validator {
+    private Validator(String tipo, int matricula) {
+      checkTipo(tipo);
+      checkMatricula(matricula);
+    }
+
+    private void checkTipo(String tipo) {
+      List<String> opcoes = Arrays.asList("CLT", "comissionado");
+
+      if (!opcoes.contains(tipo.toUpperCase())) {
+        throw new RuntimeException("Tipo de funcionário inválido");
+      }
+    }
+
+    private void checkMatricula(int matricula) {
+      if (matricula >= 1000) {
+        throw new RuntimeException("Matrícula inválida, escolha uma matricula entre 0 e 999");
+      }
+    }
   }
 }
 
 public class PrintElements {
 
   public static void mainMenu() {
-    System.out.print("""
+    System.out.println("""
         ======= SISTEMA DE FOLHA DE PAGAMENTO =======
-        1 - Cadastrar Funcionário Padrão
-        2 - Cadastrar Funcionário Comissionado
-        3 - Listar Funcionários
-        4 - Gerar Folha de Pagamento
-        5 - Sair
+        1 - Cadastrar Funcionário
+        2 - Listar Funcionários
+        3 - Sair
         Escolha uma opção: """);
 
     return;
   }
 
-  public static void funcionarioInfo(String tipoDeFuncionario, String nome, String matricula, double salario) {
+  public static void funcionarioInfo(String tipoDeFuncionario, String nome, int matricula, double salario) {
     String funcionarioInfo = """
         ======= FUNCIONARIO %s =======
         Nome: %s
         Matrícula: %s
         Salário: R$ %.2f
         ==================================
-        """.formatted(tipoDeFuncionario.toUpperCase(), nome, matricula, salario);
+        """.formatted(tipoDeFuncionario.toUpperCase(),
+        nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase(),
+        matricula,
+        salario);
     System.out.println(funcionarioInfo);
+  }
+
+  public static void cuntinuarOperacao() {
+    System.out.println("Deseja realizar outra operação? [s/n]");
   }
 
   public static void inputNome() {
