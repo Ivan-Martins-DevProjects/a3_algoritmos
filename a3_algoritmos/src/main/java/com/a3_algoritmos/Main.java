6644ca92326f25b07caa99484ffbee692b135490
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-19 22:03:57 -0300

    Criado arquivo main para testar a lógica atual

--- a3_algoritmos/src/main/java/com/a3_algoritmos/Main.java
+++ a3_algoritmos/src/main/java/com/a3_algoritmos/Main.java
@@ -1,9 +1,37 @@
 package com.a3_algoritmos;
 
+import com.a3_algoritmos.models.ListaWorkers;
+import com.a3_algoritmos.models.WorkerCLT;
+import com.a3_algoritmos.models.WorkerComissionado;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Criado com QuickMaven!");
-  }
+    WorkerComissionado workerComissionado = new WorkerComissionado(
+        "Ivan",
+        123,
+        4000,
+        10,
+        4000,
+        true,
+        true,
+        true);
 
+    WorkerCLT workerCLT = new WorkerCLT(
+        "Claudio",
+        123,
+        2000,
+        true,
+        true,
+        true);
+
+    ListaWorkers lista = new ListaWorkers();
+
+    for (int i = 0; i < 3; i++) {
+      lista.adicionarWorker(workerComissionado);
+      lista.adicionarWorker(workerCLT);
+    }
+
+    lista.listarTodosOsFuncionarios();
+  }
 }
